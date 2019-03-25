620447e7166ea79061c5871cb1b37b4a05460ad7
norie.shimoyama <info@nicona.jp>
2019-03-25 08:32:43 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,13 +83,17 @@
                 @extends('layouts.app')
 
                 @section('content')
-                    <div class="center jumbotron">
-                        <div class="text-center">
-                            <h1>Welcome to the Microposts</h1>
-                            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                    @if (Auth::check())
+                        {{ Auth::user()->name }}
+                    @else
+                        <div class="center jumbotron">
+                            <div class="text-center">
+                                <h1>Welcome to the Microposts</h1>
+                                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                            </div>
                         </div>
-                    </div>
-                <@endsection></@endsection>
+                    @endif
+                @endsection
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
